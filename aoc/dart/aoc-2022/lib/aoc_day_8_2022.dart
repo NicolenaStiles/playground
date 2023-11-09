bc4c517250d557bf70c1d89ff5275e9492f74a06
NicolenaStiles <nicolena.stiles@gmail.com>
2023-11-08 19:29:01 -0500

    WOW OK I do not wanna fuck up that generate statement ever again. Fuck.

--- aoc/dart/aoc-2022/lib/aoc_day_8_2022.dart
+++ aoc/dart/aoc-2022/lib/aoc_day_8_2022.dart
@@ -19,44 +19,48 @@ void partOne() {
        List<int> heights = treeline.split("").map((e) => int.parse(e)).toList();
        treeHeights.add(heights);
    }
-    // isVisible tracks if the trees are visible from outside the grid
-    List<bool> temp = List.generate(readIn[0].length, (index) => false);
-    List<List<bool>> isVisible = List.generate(readIn.length, (index) => temp);
 
-    for (var entry in isVisible) {
-        print(entry);
+    // isVisible tracks if the trees are visible from outside the grid
+    List<List<bool>> isVisible = []; 
+    for (int y = 0; y < readIn.length; y++) {
+        List<bool> temp = []; 
+        for (int x = 0; x < readIn[0].length; x++) {
+            temp.add(false);
+        }
+        isVisible.add(temp);
     }
+
+    // make sure the outside edge is marked as visible
 
     // figure out if taller in any direction
     for (int y = 1; y < treeHeights[0].length - 1; y++) {
         for (int x = 1; x < treeHeights.length - 1; x++) {
             int currHeight = treeHeights[y][x];
-            print("flipped: $x,$y:${treeHeights[y][x]}");
             // checking the north
             bool visibleNorth = true;
             for (int numNorth = 0; numNorth < y; numNorth++) {
-                if (currHeight < treeHeights[numNorth][x]) {
+                if (currHeight <= treeHeights[numNorth][x]) {
                     visibleNorth = false;
                 }
             }
             // checking the south 
             bool visibleSouth = true;
-            for (int numSouth = y + 1; numSouth < treeHeights.length - 2; numSouth++) {
-                if (currHeight < treeHeights[numSouth][x]) {
+            for (int numSouth = y + 1; numSouth < treeHeights.length; numSouth++) {
+                if (currHeight <= treeHeights[numSouth][x]) {
                     visibleSouth = false;
                 }
             }
             // checking the west 
             bool visibleWest = true;
             for (int numWest = 0; numWest < x; numWest++) {
-                if (currHeight < treeHeights[y][numWest]) {
+                if (currHeight <= treeHeights[y][numWest]) {
                     visibleWest = false;
                 }
             }
             // checking the east 
             bool visibleEast = true;
-            for (int numEast = x + 1; numEast < treeHeights[0].length - 2; numEast++) {
-                if (currHeight < treeHeights[y][numEast]) {
+            for (int numEast = x + 1; numEast < treeHeights[0].length; numEast++) {
+                if (currHeight <= treeHeights[y][numEast]) {
                     visibleEast = false;
                 }
             }
@@ -64,11 +68,5 @@ void partOne() {
                 isVisible[y][x] = true;
             }
         }
-    }
-    for (var entry in isVisible) {
-        print(entry);
-    }
-    for (var treeline in treeHeights) {
-        print(treeline);
     }
 }
