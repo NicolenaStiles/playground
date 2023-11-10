aea56d1bf46cc2cac071d93b4a2db084d89e86d6
NicolenaStiles <nicolena.stiles@gmail.com>
2023-11-09 23:08:29 -0500

    Cleanup done but for some reason now it does not work

--- aoc/dart/aoc-2022/lib/aoc_day_9_2022.dart
+++ aoc/dart/aoc-2022/lib/aoc_day_9_2022.dart
@@ -13,74 +13,6 @@ List<List> readInput(String filename){
     return movements;
 }
 
-List<List<int>> moveUp(List<int> head, List<int> tail) {
-    List<int> currentHead = head;
-    List<int> currentTail = tail;
-    if (!(currentHead[0] == currentTail[0] && currentHead[1] == currentTail[1])) {
-        if(currentTail[1] < currentHead[1]) {
-            if(currentTail[0] > currentHead[0]) {
-                currentTail[0]--;
-            } else if (currentTail[0] < currentHead[0]) {
-                currentTail[0]++;
-            }
-            currentTail[1]++;
-        }
-    }
-    currentHead[1]++;
-    return [currentHead, currentTail];
-}
-
-List<List<int>> moveDown(List<int> head, List<int> tail) {
-    List<int> currentHead = head;
-    List<int> currentTail = tail;
-    if (!(currentHead[0] == currentTail[0] && currentHead[1] == currentTail[1])) {
-        if(currentTail[1] > currentHead[1]) {
-            if(currentTail[0] > currentHead[0]) {
-                currentTail[0]--;
-            } else if (currentTail[0] < currentHead[0]) {
-                currentTail[0]++;
-            }
-            currentTail[1]--;
-        }
-    }
-    currentHead[1]--;
-    return [currentHead, currentTail];
-}
-
-List<List<int>> moveLeft(List<int> head, List<int> tail) {
-    List<int> currentHead = head;
-    List<int> currentTail = tail;
-    if (!(currentHead[0] == currentTail[0] && currentHead[1] == currentTail[1])) {
-        if(currentTail[0] > currentHead[0]) {
-            if(currentTail[1] > currentHead[1]) {
-                currentTail[1]--;
-            } else if (currentTail[1] < currentHead[1]) {
-                currentTail[1]++;
-            }
-            currentTail[0]--;
-        }
-    }
-    currentHead[0]--;
-    return [currentHead, currentTail];
-}
-
-List<List<int>> moveRight(List<int> head, List<int> tail) {
-    List<int> currentHead = head;
-    List<int> currentTail = tail;
-    if (!(currentHead[0] == currentTail[0] && currentHead[1] == currentTail[1])) {
-        if(currentTail[0] < currentHead[0]) {
-            if(currentTail[1] > currentHead[1]) {
-                currentTail[1]--;
-            } else if (currentTail[1] < currentHead[1]) {
-                currentTail[1]++;
-            }
-            currentTail[0]++;
-        }
-    }
-    currentHead[0]++;
-    return [currentHead, currentTail];
-}
-
 // move head and return new position
 List<int> moveHead(String move, List<int> position) {
     List<int> currentHead = position;
@@ -109,9 +41,11 @@ List<int> moveHead(String move, List<int> position) {
 
 // move follower closer to head
 List<int> moveFollower(List<int> head, List<int> tail) {
+
     List<int> currentTail = tail;
     int diffX = head[0] - tail[0];
     int diffY = head[1] - tail[1];
+
     if(diffX.abs() > 1) {
         if(diffX > 0) {
             currentTail[0]++;
@@ -126,6 +60,7 @@ List<int> moveFollower(List<int> head, List<int> tail) {
             }
         }
     }
+
     if(diffY.abs() > 1) {
         if(diffY > 0) {
             currentTail[1]++;
@@ -140,162 +75,35 @@ List<int> moveFollower(List<int> head, List<int> tail) {
             }
         }
     }
+
     return currentTail;
-}
 
-// Executes a single move instruction for n knots.
-// inputs:
-// String move: string indicating which move is to be executed (U,D,L,R);
-// List<List<int>> position: list (length n) of positions (x,y) for n knots
-// output:
-// List<List<int>> of n knot positions after move is executed
-List<List<int>> singleMove(String move, List<List<int>> position) {
-    List<List<int>> newPositions = [];
-    for(int i = 0; i < position.length - 1; i++){
-        List<int> currentHead = position[i];
-        List<int> currentTail = position[i+1];
-        print("$i iteration has head: $currentHead tail: $currentTail");
-
-        switch (move) {
-
-            case 'U': 
-                break;
-
-            case 'D': 
-                break;
-
-            case 'L': 
-                break;
-
-            case 'R': 
-                break;
-
-            default:
-                print("ERROR reading movement direction!");
-        }
-    }
-
-    return newPositions;
-}
-
-// reads in a list of movements and applies them based on current head/tail pos
-// inputs:
-// moves[0] == U,D,L,R, moves[1] == # movements (as int)
-// head[0] == x, head[1] == y
-// tail[0] == x, tail[1] == y
-// outputs:
-// Map (with keys 'H' and 'T') of List<List<int>>, where each list 
-// represents coordinates after a single move
-Map executeMoves(List moves, List<int> head, List<int> tail) {
-    Map path = {};
-    path['H'] = [];
-    path['T'] = [];
-    List<int> headPos = head;
-    List<int> tailPos = tail;
-    for (int i = 0; i < moves[1]; i++) {
-        // if head/tail are not in the exact same square...
-        if (!(headPos[0] == tailPos[0] && headPos[1] == tailPos[1])) {
-
-            switch (moves[0]) {
-
-                  case 'U': 
-                    if(tailPos[1] < headPos[1]) {
-                        if(tailPos[0] > headPos[0]) {
-                            tailPos[0]--;
-                        } else if (tailPos[0] < headPos[0]) {
-                            tailPos[0]++;
-                        }
-                        tailPos[1]++;
-                    }
-                    headPos[1]++;
-                    break;
-
-                  case 'D': 
-                     if(tailPos[1] > headPos[1]) {
-                        if(tailPos[0] > headPos[0]) {
-                            tailPos[0]--;
-                        } else if (tailPos[0] < headPos[0]) {
-                            tailPos[0]++;
-                        }
-                        tailPos[1]--;
-                    }
-                    headPos[1]--;
-                    break;
-
-                  case 'L': 
-                     if(tailPos[0] > headPos[0]) {
-                        if(tailPos[1] > headPos[1]) {
-                            tailPos[1]--;
-                        } else if (tailPos[1] < headPos[1]) {
-                            tailPos[1]++;
-                        }
-                        tailPos[0]--;
-                    }
-                    headPos[0]--;
-                    break;
-
-                  case 'R': 
-                     if(tailPos[0] < headPos[0]) {
-                        if(tailPos[1] > headPos[1]) {
-                            tailPos[1]--;
-                        } else if (tailPos[1] < headPos[1]) {
-                            tailPos[1]++;
-                        }
-                        tailPos[0]++;
-                    }
-                    headPos[0]++;
-                    break;
-
-                  default:
-                    print("ERROR reading movement direction!");
-            }
-        // if head/tail are in the same square, only head moves
-        } else {
-            switch (moves[0]) {
-                  case 'U': 
-                    headPos[1]++;
-                    break;
-                  case 'D': 
-                    headPos[1]--;
-                    break;
-                  case 'L': 
-                    headPos[0]--;
-                    break;
-                  case 'R': 
-                    headPos[0]++;
-                    break;
-                  default:
-                    print("ERROR reading movement direction!");
-            }
-        }
-        path['H'].add([headPos[0],headPos[1]]);
-        path['T'].add([tailPos[0],tailPos[1]]);
-    }
-    return path;
 }
 
 void partOne() {
 
+    // reading in data as usual
     List<List> movements = readInput('input-day-9');
 
-    Map moveTracker = {};
-    moveTracker['H'] = [[0,0]];
-    moveTracker['T'] = [[0,0]];
+    // for real this time
+    List<int> currHead = [0,0];
+    List<int> currTail = [0,0];
+    Set pathsHead = {};
+    Set pathsTail= {};
+    pathsHead.add("0,0");
+    pathsTail.add("0,0");
 
-    Set uniqueSquaresHead = {};
-    Set uniqueSquaresTail = {};
-    for (var move in movements) {
-        moveTracker = executeMoves(move, moveTracker['H'].last, moveTracker['T'].last);
-        for (var h in moveTracker['H']) {
-            String headString = "${h[0]},${h[1]}";
-            uniqueSquaresHead.add(headString);
-        }            
-        for (var t in moveTracker['T']) {
-            String tailString = "${t[0]},${t[1]}";
-            uniqueSquaresTail.add(tailString);
-        }            
+    for(var move in movements) {
+        for(int i = 0; i < move[1]; i++) {
+            currHead = moveHead(move[0], currHead);
+            currTail = moveFollower(currHead, currTail);
+            String headPath = "${currHead[0]},${currHead[1]}";
+            pathsHead.add(headPath);
+            String tailPath = "${currTail[0]},${currTail[1]}";
+            pathsTail.add(tailPath);
+        }
     }
 
-    print(uniqueSquaresTail.length);
+   print(pathsTail.length);
 
 }
--- aoc/dart/aoc-2022/test/aoc_2022_test.dart
+++ aoc/dart/aoc-2022/test/aoc_2022_test.dart
@@ -3,55 +3,7 @@ import 'package:test/test.dart';
 
 void main() {
     test('day-9-part-1', () {
-        // Made sure load-in process is correct
-        List<List> movements = readInput('input-day-9-test');
-        expect(movements[0].length, 2);
-        expect(movements[0][0], 'R');
-        expect(movements[0][1], 4);
-        
-        // test out movement logic
-        List<int> head = [0,0];
-        List<int> tail = [0,0];
 
-        // First movement is R 4
-        // Expect Head = 4,0
-        // Expect Tail = 3,0
-        Map movementTracker = executeMoves(movements[0], head, tail);
-        expect(movementTracker['H'].last, [4,0]);
-        expect(movementTracker['T'].last, [3,0]);
-
-        // Second movement is U 4
-        // Expect Head = 4,4
-        // Expect Tail = 4,3
-        Map movementTracker2 = executeMoves(movements[1], movementTracker['H'].last, movementTracker['T'].last);
-        expect(movementTracker2['H'].last, [4,4]);
-        expect(movementTracker2['T'].last, [4,3]);
-
-        // Skip some steps and test out the uniqueness process w/sets
-        Map moveTracker = {};
-        moveTracker['H'] = [[0,0]];
-        moveTracker['T'] = [[0,0]];
-
-        Set uniqueSquaresHead = {};
-        Set uniqueSquaresTail = {};
-        for (var move in movements) {
-            moveTracker = executeMoves(move, moveTracker['H'].last, moveTracker['T'].last);
-            for (var h in moveTracker['H']) {
-                String headString = "${h[0]},${h[1]}";
-                uniqueSquaresHead.add(headString);
-            }            
-            for (var t in moveTracker['T']) {
-                String tailString = "${t[0]},${t[1]}";
-                uniqueSquaresTail.add(tailString);
-            }            
-        }
-        // Correct solution as per website
-        expect(uniqueSquaresTail.length, 13);
-
-    });
-
-    test('day-9-part-2', () {
-        
         // reading in data as usual
         List<List> movements = readInput('input-day-9-test');
 
@@ -60,8 +12,10 @@ void main() {
         List<int> currTail = [0,0];
         Set pathsHead = {};
         Set pathsTail= {};
+        pathsHead.add("0,0");
+        pathsTail.add("0,0");
+
         for(var move in movements) {
-            print("Move: $move");
             for(int i = 0; i < move[1]; i++) {
                 currHead = moveHead(move[0], currHead);
                 currTail = moveFollower(currHead, currTail);
@@ -71,8 +25,13 @@ void main() {
                 pathsTail.add(tailPath);
             }
         }
+
         expect(pathsTail.length, 13);
 
+   });
+
+    test('day-9-part-2', () {
+        
         /*
         // Starting info for knots
         int numKnots = 10;
