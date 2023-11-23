506e051e003c81cd1a689b1551df50ce1e59061a
NicolenaStiles <nicolena.stiles@gmail.com>
2023-11-22 20:38:45 -0500

    On hover effects working and logic for rows kinda shaping up\?

--- nicolena_dot_net/lib/main.dart
+++ nicolena_dot_net/lib/main.dart
@@ -46,60 +46,127 @@ class MenuItemView {
 
 class _HomePageState extends State<HomePage> {
 
+  List<Container> _navList = [];
+  List<String> _navListEntires = ['About', 'Blog', 'Stats', 'Contact'];
+
+  Color _hoverColor = Colors.lime;
+
+  TextStyle _hoverStyle = TextStyle(fontSize: 18, color: Colors.white);
+
+
+  void _changeColors(Color c) {
+    setState((){
+      _hoverColor = c;
+    }
+    );
+  }
+
+  void _changeStyle(bool _isHover) {
+    setState(() {
+      if (_isHover) {
+        _hoverStyle = TextStyle(
+          shadows: [
+            Shadow(
+              blurRadius: 10,
+              color: Colors.teal,
+              offset: Offset(5, 5),
+            )
+          ],
+          fontSize: 18,
+          color: Colors.teal,
+        );
+      } else {
+        _hoverStyle = TextStyle(fontSize: 18, color: Colors.white);
+      }
+    });
+  }
+
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold( 
       body: Center(
-        child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          const Image (
-            // TODO: scale this correctly! It's way too big!
-            image: AssetImage('assets/asteroids_logo.png'),
-          ),
-          Row(
-            // TODO: constrain this better! Also need to be icons!
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              Container(
-                color: Colors.pink,
-                padding: const EdgeInsets.all(20.0),
-                child: const Text(
-                  'About',
-                  style: TextStyle(fontSize: 24),
+        child: Column( 
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget> [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+
+                Container(
+                  color: Colors.pink,
+                  alignment: Alignment.center,
+                  width: 200,
+                  height: 50,
+                  child: Text(
+                          'About',
+                          style: TextStyle(fontSize: 24),
+                          ),
                 ),
-              ),
-              Container(
-                height: 80,
-                child: VerticalDivider(color: Colors.red)
-              ),
-              Container(
-                color: Colors.pink,
-                padding: const EdgeInsets.all(20.0),
-                child: const Text(
-                  'Blog',
-                  style: TextStyle(fontSize: 24),
+
+                Container(
+                  height: 80,
+                  child: VerticalDivider(color: Colors.lime),
                 ),
-              ),
-              Container(
-                height: 80,
-                child: VerticalDivider(color: Colors.red)
-              ),
-              Container(
-                color: Colors.pink,
-                width: 200,
-                height: 100,
-                child: const Text(
-                  'Contact',
-                  style: TextStyle(fontSize: 24),
-                  textAlign: TextAlign.center
+
+                Container(
+                  color: Colors.pink,
+                  alignment: Alignment.center,
+                  width: 200,
+                  height: 50,
+                  child: Text(
+                          'Blog',
+                          style: TextStyle(fontSize: 24),
+                          ),
                 ),
-              ),
-            ],
-          )
-        ]
-        )
-      )
+
+                Container(
+                  height: 80,
+                  child: VerticalDivider(color: Colors.lime),
+                ),
+
+                Container(
+                  color: Colors.pink,
+                  alignment: Alignment.center,
+                  width: 200,
+                  height: 50,
+                  child: Text(
+                          'Contact',
+                          style: TextStyle(fontSize: 24),
+                          ),
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container( 
+                  color: Colors.black,
+                  alignment: Alignment.center,
+                  width: 300,
+                  height: 100,
+                  child: MouseRegion(
+                    onEnter: (_) {                      
+                      _changeStyle(true);
+                    },
+
+                    onExit: (_) {
+                      _changeStyle(false);
+                    },
+
+                    child: Text( 
+                      'This is the mouse area testing location!',
+                      style: _hoverStyle,
+                    )
+                  ),
+
+                )
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
