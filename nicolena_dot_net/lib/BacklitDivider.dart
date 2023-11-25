b22eb808b7690adc7a3ef6408508970f7fa19be3
NicolenaStiles <nicolena.stiles@gmail.com>
2023-11-24 23:39:19 -0500

    Brain mush I forgot the actual refactor

---
+++ nicolena_dot_net/lib/BacklitDivider.dart
@@ -0,0 +1,32 @@
+
+import 'package:flutter/material.dart';
+
+class BacklitDivider extends StatelessWidget {
+
+  const BacklitDivider({
+      super.key, 
+      });
+
+  @override
+  Widget build(BuildContext context) {
+    return 
+      Container( 
+        height: 25,
+        width: 4,
+        decoration: const BoxDecoration(
+        boxShadow: [
+          BoxShadow(
+            color: Colors.cyan,
+            blurRadius: 2,
+            offset: Offset(0,0),
+          ),
+        ],
+      ),
+      child: const SizedBox(
+        height: 25,
+        child: VerticalDivider(color: Colors.cyan),
+      ),
+    );
+  }
+
+}
