823c9bccec7df7fb1cf14c225f101cec4fd16194
NicolenaStiles <nicolena.stiles@gmail.com>
2023-11-29 21:50:28 -0500

    Named routing without the go router package

--- flutter_tutorials/lib/main.dart
+++ flutter_tutorials/lib/main.dart
@@ -10,97 +10,129 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+
     return MaterialApp(
       title: 'Flutter Tutorials',
       initialRoute: '/',
       routes: {
-        '/': (context) => const HomePage(title: 'Home screen'),
-        '/second': (context) => const SecondScreen(),
+
+        '/': (context) => const HomePage(),
+        '/about': (context) => const AboutScreen(),
+        '/blog': (context) => const BlogScreen(),
+        '/contact': (context) => const ContactScreen(),
       },
       theme: ThemeData(
+
         colorScheme: const ColorScheme.dark(),
         useMaterial3: true,
       ),
-      home: const HomePage(title: 'Flutter Tutorials Home Page'),
     );
   }
 }
 
-class HomePage extends StatefulWidget {
-  const HomePage({super.key, required this.title});
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
 
-  final String title;
-
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
+  @override 
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+      appBar: AppBar( 
+        title: const Text('Home Page'),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+        child: Column( 
+          children: [
+
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/about');
+              }, child: const Text('About page'),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/blog');
+              }, child: const Text('Blog'),
             ),
+
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/contact');
+              }, child: const Text('Contact info page'),
+            )
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
+
+class AboutScreen extends StatelessWidget {
+  const AboutScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold ( 
+
+      appBar: AppBar ( 
+        title: const Text('About Page'),
+      ),
+      body: Center (
+
+        child: ElevatedButton(
+          onPressed: () {
+          Navigator.pop(context);
+          }, child: const Text('Back to homepage'),
+        )
+
+      ),
+    );
+  }
+}
+
+class BlogScreen extends StatelessWidget {
+  const BlogScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold ( 
+
+      appBar: AppBar ( 
+        title: const Text('Blog Page'),
+      ),
+      body: Center (
+
+        child: ElevatedButton(
+          onPressed: () {
+          Navigator.pop(context);
+          }, child: const Text('Back to homepage'),
+        )
+
+      ),
+    );
+  }
+}
+
+class ContactScreen extends StatelessWidget {
+  const ContactScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold ( 
+
+      appBar: AppBar ( 
+        title: const Text('Contact Page'),
+      ),
+      body: Center (
+
+        child: ElevatedButton(
+          onPressed: () {
+          Navigator.pop(context);
+          }, child: const Text('Back to homepage'),
+        )
+
+      ),
+    );
+  }
+}
+
