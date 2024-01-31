ca021937f2683536dcca3a37a43423d44744a311
NicolenaStiles <nicolena.stiles@gmail.com>
2024-01-31 16:18:38 -0500

    Saving state before move to platform-sensitive configs

--- asteroids/lib/main.dart
+++ asteroids/lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
 
 import 'src/asteroids.dart';
-// import 'reference/asteroids.dart';
 
 void main() {
   final game = Asteroids();
