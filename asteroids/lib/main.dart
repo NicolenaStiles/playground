570a04cdb5edc04660006b5fd231727883dd929d
NicolenaStiles <nicolena.stiles@gmail.com>
2024-02-02 15:11:42 -0500

    TODOs outlined and ready to start work.

--- asteroids/lib/main.dart
+++ asteroids/lib/main.dart
@@ -3,6 +3,15 @@ import 'package:flutter/material.dart';
 
 import 'src/asteroids.dart';
 
+// TODO: 1. Create static display test image layout 
+// Should include ship, all asteroid sizes, and HUD componenets
+
+// TODO: 2. Create config file to handle independent sizing for each
+// Should also go into the guts of each object and allow them to resize
+// (Entirely different gameplay loop? How handle this?)
+
+// TODO: 3. Handle resize/screen rotation (landscape vs portrait)
+
 void main() {
   final game = Asteroids();
   runApp(GameWidget(game: game));
