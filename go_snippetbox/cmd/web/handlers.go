9a86bfcc065069f8c8c6e017aa39d37a7ffbba14
NicolenaStiles <nicolena.stiles@gmail.com>
2024-03-14 17:02:05 -0400

    Saving progress up through 2.10

--- go_snippetbox/cmd/web/handlers.go
+++ go_snippetbox/cmd/web/handlers.go
@@ -2,19 +2,50 @@ package main
 
 import (
   "fmt"
+  "html/template"
+  "log"
   "net/http"
   "strconv"
 )
 
+// METHOD: ANY
+// PATTERN: "/"
+// HANDLER: home 
+// ACTION: Display the home page 
 func home(w http.ResponseWriter, r *http.Request) {
   if r.URL.Path != "/" {
     http.NotFound(w,r)
     return
   }
 
-  w.Write([]byte ("Hello from Snippetbox"))
+  // init a slice containing paths to templating files
+  files := []string{
+    "./ui/html/home.page.tmpl",
+    "./ui/html/base.layout.tmpl",
+    "./ui/html/footer.partial.tmpl",
+  }
+
+  // Read the template files into a template set.
+  // if theres an error, log it internally and send a generic 500 error to user
+  ts, err := template.ParseFiles(files...)
+  if err != nil {
+    log.Println(err.Error())
+    http.Error(w, "Internal Server Error", 500)
+    return
+  }
+
+  // write template content as the response body
+  err = ts.Execute(w, nil)
+  if err != nil {
+    log.Println(err.Error())
+    http.Error(w, "Internal Server Error", 500)
+  }
 }
 
+// METHOD: ANY
+// PATTERN: "/snippet?id=1"
+// HANDLER: showSnippet 
+// ACTION: Display a specific snippet
 func showSnippet(w http.ResponseWriter, r *http.Request) {
   id, err := strconv.Atoi(r.URL.Query().Get("id"))
   if err != nil || id < 1 {
@@ -25,6 +56,10 @@ func showSnippet(w http.ResponseWriter, r *http.Request) {
   fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
 }
 
+// METHOD: POST 
+// PATTERN: "/snippet/create"
+// HANDLER: createSnippet 
+// ACTION: Create a new snippet
 func createSnippet(w http.ResponseWriter, r *http.Request) {
   if r.Method != http.MethodPost {
     w.Header().Set("Allow", http.MethodPost)
--- go_snippetbox/cmd/web/main.go
+++ go_snippetbox/cmd/web/main.go
@@ -11,6 +11,19 @@ func main() {
   mux.HandleFunc("/snippet", showSnippet)
   mux.HandleFunc("/snippet/create", createSnippet)
 
+  // METHOD: ANY 
+  // PATTERN: "/static/"
+  // HANDLER: http.FileServer
+  // ACTION: Serve a specific static file
+  //
+  // create a file server which serves files out of the "./ui/static" 
+  // directory. note that filepath is relative!
+  fileServer := http.FileServer(http.Dir("./ui/static/"))
+
+  // use mux.Handle() to regester the file server as the handler for 
+  // all URL paths that start with "/static/"
+  mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
   log.Println("Starting server on :4000...")
   err := http.ListenAndServe(":4000", mux)
   log.Fatal(err)
